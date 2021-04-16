181aca85159a8398cc1012fe2eae87b36cc6fc53
FedorSabeshkin <fsabeshkin@mail.ru>
2021-04-16 20:01:21 +0300

    prepare develop remove method

---
+++ src/com/sabeshkin/linkedlist/LinkedList.java
@@ -0,0 +1,87 @@
+package com.sabeshkin.linkedlist;
+
+import java.util.*;
+
+public class LinkedList {
+	public Node head;
+	public Node tail;
+
+	public LinkedList() {
+		head = null;
+		tail = null;
+	}
+
+	public void addInTail(Node item) {
+		if(this.head == null)
+			this.head = item;
+		else
+			this.tail.next = item;
+		this.tail = item;
+	}
+
+	public Node find(int value) {
+		Node node = this.head;
+		while(node != null){
+			if(node.value == value)
+				return node;
+			node = node.next;
+		}
+		return null;
+	}
+
+	public ArrayList<Node> findAll(int _value) {
+		ArrayList<Node> nodes = new ArrayList<Node>();
+		// здесь будет ваш код поиска всех узлов
+		return nodes;
+	}
+
+	public boolean remove(int _value) {
+		this.find(_value);
+		
+		Node node = this.head;
+		while(node != null){
+			if(node.next != null){
+				if(node.next.value == _value){
+					Node removedNode = node.next;
+					Node prevRemovedNode = node;
+					Node nextRemovedNode = removedNode.next;
+					prevRemovedNode.next = removedNode.next;
+					return true;	
+				}
+			}
+			node = node.next;
+		}
+		// здесь будет ваш код удаления одного узла по заданному значению
+		return false; // если узел был удалён
+	}
+
+	public void removeAll(int _value) {
+		// здесь будет ваш код удаления всех узлов по заданному значению
+	}
+
+	public void clear() {
+		// здесь будет ваш код очистки всего списка
+	}
+
+	public int count() {
+		return 0; // здесь будет ваш код подсчёта количества элементов в списке
+	}
+
+	public void insertAfter(Node _nodeAfter, Node _nodeToInsert) {
+		// здесь будет ваш код вставки узла после заданного
+
+		// если _nodeAfter = null ,
+		// добавьте новый элемент первым в списке
+	}
+
+}
+
+class Node {
+	public int value;
+	public Node next;
+
+	public Node(int _value) {
+		value = _value;
+		next = null;
+	}
+}
---
+++ src/tests/LinkedListTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LinkedListTest {
+
+	
+
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+
+}
