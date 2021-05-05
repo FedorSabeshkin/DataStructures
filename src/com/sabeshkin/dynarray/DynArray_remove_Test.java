eea6e153f1b4081b4601a41634ff839078a3571f
FedorSabeshkin <fsabeshkin@mail.ru>
2021-05-05 09:05:01 +0300

    add tests for remove(index)

--- src/com/sabeshkin/dynarray/DynArray_remove_Test.java
+++ src/com/sabeshkin/dynarray/DynArray_remove_Test.java
@@ -10,6 +10,83 @@ public class DynArray_remove_Test {
 	Integer item;
 	
 	@Test
+	public void test_Remove_getZeroElementWithoutMethod() {
+		for (int i = 0; i < 10; i++){
+			item = new Integer(i);
+			dynArray.append(item);
+		}
+		dynArray.remove(0);
+		assertEquals(
+				null,
+				dynArray.array[0]);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_Remove_unexistElementIndex_isIllegalArgumentException() {
+		for (int i = 0; i < 17; i++){
+			item = new Integer(i);
+			dynArray.append(item);
+		}
+		dynArray.remove(6);
+		dynArray.remove(5);
+		assertEquals(
+				new Integer(23),
+				dynArray.getItem(23));
+	}
+	
+	@Test
+	public void test_Remove_capacityIsSixteen() {
+		for (int i = 0; i < 17; i++){
+			item = new Integer(i);
+			dynArray.append(item);
+		}
+		
+		for (int i = 0; i < 10; i++){
+			dynArray.remove(i);
+		}
+		assertEquals(
+				16,
+				dynArray.capacity);
+	}
+	
+	@Test
+	public void test_Remove_countIsSeven() {
+		for (int i = 0; i < 17; i++){
+			item = new Integer(i);
+			dynArray.append(item);
+		}
+		
+		for (int i = 0; i < 10; i++){
+			dynArray.remove(i);
+		}
+		assertEquals(
+				7,
+				dynArray.count);
+	}
+
+	@Test
+	public void test_Remove_zeroElementIsNull() {
+		for (int i = 0; i < 10; i++){
+			item = new Integer(i);
+			dynArray.append(item);
+		}
+		dynArray.remove(0);
+		assertEquals(
+				null,
+				dynArray.getItem(0));
+	}
+
+	DynArray<Integer> dynArrayEmpty = new DynArray<Integer>(Integer.class);
+
+	@Test
+	public void test_Remove_fromEmptyArr_fifthElementIsNull() {
+		dynArrayEmpty.remove(5);
+		assertEquals(
+				null,
+				dynArrayEmpty.getItem(5));
+	}
+
+	@Test
 	public void test_Remove_fifthElementIsNull() {
 		for (int i = 0; i < 10; i++){
 			item = new Integer(i);
@@ -20,7 +97,7 @@ public class DynArray_remove_Test {
 				null,
 				dynArray.getItem(5));
 	}
-	
+
 	@Test
 	public void test_Remove_sixthElementIsNull() {
 		for (int i = 0; i < 17; i++){
@@ -33,7 +110,7 @@ public class DynArray_remove_Test {
 				null,
 				dynArray.getItem(6));
 	}
-	
+
 	@Test
 	public void test_Remove_capacityIsTwentyOne() {
 		for (int i = 0; i < 17; i++){
@@ -46,7 +123,7 @@ public class DynArray_remove_Test {
 				21,
 				dynArray.capacity);
 	}
-	
+
 	@Test
 	public void test_Remove_seventhElementIsNull() {
 		for (int i = 0; i < 18; i++){
@@ -58,7 +135,7 @@ public class DynArray_remove_Test {
 				null,
 				dynArray.getItem(7));
 	}
-	
+
 	@Test
 	public void test_Remove_capacityIsThirtyTwo() {
 		for (int i = 0; i < 18; i++){
@@ -70,7 +147,7 @@ public class DynArray_remove_Test {
 				32,
 				dynArray.capacity);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void test_Remove_negativeIndex_illegalArgumentException() {
 		for (int i = 0; i < 16; i++){
@@ -82,7 +159,7 @@ public class DynArray_remove_Test {
 				new Integer(10),
 				dynArray.getItem(6));
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void test_Remove_oneHundredIndex_illegalArgumentException() {
 		for (int i = 0; i < 16; i++){
@@ -94,7 +171,7 @@ public class DynArray_remove_Test {
 				new Integer(10),
 				dynArray.getItem(6));
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void test_Remove_outOfCapacityIndex_illegalArgumentException() {
 		for (int i = 0; i < 16; i++){
