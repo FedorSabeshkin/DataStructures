7270a70d4ce14216b4d4fbe6e9211b6a464291a3
FedorSabeshkin <fsabeshkin@mail.ru>
2021-05-11 22:45:08 +0300

    add Stack class

---
+++ src/stack/Stack.java
@@ -0,0 +1,39 @@
+package stack;
+
+import java.util.*;
+
+public class Stack<T> {
+	LinkedList<T> linkedList;
+	Class clazz;
+	public Stack() {
+		linkedList = new LinkedList<>();
+		// инициализация внутреннего хранилища стека
+	}
+
+	public int size() {
+		
+		// размер текущего стека
+		return linkedList.size();
+	}
+
+	public T pop() {
+		try{
+			return linkedList.pop();
+		}catch(NoSuchElementException exception){
+			return null;
+		}
+		// ваш код
+		// return null; // если стек пустой
+	}
+
+	public void push(T val) {
+		linkedList.push(val);
+		// ваш код
+	}
+
+	public T peek() {
+		// ваш код
+		return linkedList.peek();
+		// return null; // если стек пустой
+	}
+}
