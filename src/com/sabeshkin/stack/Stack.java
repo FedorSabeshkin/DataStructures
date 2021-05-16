2039896c1fd00ed044f9bf045320ab6b832a8fd7
FedorSabeshkin <fsabeshkin@mail.ru>
2021-05-16 16:47:28 +0300

    add parenthesisValidator

--- src/com/sabeshkin/stack/Stack.java
+++ src/com/sabeshkin/stack/Stack.java
@@ -4,11 +4,11 @@ import java.util.*;
 
 public class Stack<T> {
 	LinkedList<T> linkedList;
+
 	public Stack() {
-		// инициализация внутреннего хранилища стека
 		linkedList = new LinkedList<>();
 	}
-	
+
 	public int size() {
 		return linkedList.size();
 	}
@@ -16,19 +16,43 @@ public class Stack<T> {
 	public T pop() {
 		try{
 			return linkedList.pop();
-		}catch(NoSuchElementException exception){
+		} catch (NoSuchElementException exception){
 			return null;
 		}
-		// return null; // если стек пустой
 	}
 
 	public void push(T val) {
 		linkedList.push(val);
-		// ваш код
 	}
 
 	public T peek() {
 		return linkedList.peek();
-		// return null; // если стек пустой
 	}
+
+	public static boolean parenthesisValidator(String inString, Stack stack) {
+		int i = 0;
+		Stack openParenthesisStack = new Stack();
+		Stack closeParenthesisStack = new Stack();
+		// Character openParenthesis = new Character('(');
+		boolean isOpenParenthesis = false;
+		while(i < inString.length()){
+			char openParenthesis = '(';
+			char closeParenthesis = ')';
+			while(inString.charAt(i) == openParenthesis){
+				openParenthesisStack.push(inString.charAt(i));
+				isOpenParenthesis = true;
+				i++;
+			}
+			while(isOpenParenthesis){
+
+			}
+			while(inString.charAt(i) == closeParenthesis){
+				closeParenthesisStack.push(inString.charAt(i));
+				i++;
+			}
+			stack.push(inString.charAt(i));
+		}
+		return (!isOpenParenthesis && (closeParenthesisStack.size() == openParenthesisStack.size()));
+	}
+
 }
---
+++ src/com/sabeshkin/stack/validateParenthesis_tests.java
@@ -0,0 +1,64 @@
+package com.sabeshkin.stack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class validateParenthesis_tests {
+
+	@Test
+	public void test_isFalse() {
+		Stack stack = new Stack();
+		String string = new String("())(");
+		assertEquals(
+				false,
+				stack.parenthesisValidator(string, stack));
+	}
+	
+	@Test
+	public void test_second_isFalse() {
+		Stack stack = new Stack();
+		String string = new String("))((");
+		assertEquals(
+				false,
+				stack.parenthesisValidator(string, stack));
+	}
+	
+	@Test
+	public void test_third_isFalse() {
+		Stack stack = new Stack();
+		String string = new String("((())");
+		assertEquals(
+				false,
+				stack.parenthesisValidator(string, stack));
+	}
+	
+	@Test
+	public void test_fourth_isTrue() {
+		Stack stack = new Stack();
+		String string = new String("(()((())()))");
+		assertEquals(
+				true,
+				stack.parenthesisValidator(string, stack));
+	}
+	
+	@Test
+	public void test_fifth_isTrue() {
+		Stack stack = new Stack();
+		String string = new String("()");
+		assertEquals(
+				true,
+				stack.parenthesisValidator(string, stack));
+	}
+	
+	@Test
+	public void test_together_isTrue() {
+		Stack stack = new Stack();
+		String string = new String("()()");
+		assertEquals(
+				true,
+				stack.parenthesisValidator(string, stack));
+	}
+
+
+}
