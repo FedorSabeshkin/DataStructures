61b77053b2564acf5adc01e77acdfe9b2d4343a8
FedorSabeshkin <fsabeshkin@mail.ru>
2021-07-24 21:58:42 +0300

    first realization

--- src/com/sabeshkin/bloomfilter/BloomFilter.java
+++ src/com/sabeshkin/bloomfilter/BloomFilter.java
@@ -2,34 +2,54 @@ package com.sabeshkin.bloomfilter;
 
 public class BloomFilter {
 	public int filter_len;
-
+	private int[] filter;
 	public BloomFilter(int f_len) {
 		filter_len = f_len;
+		filter = new int[filter_len];
 		// создаём битовый массив длиной f_len ...
 	}
 
-	// хэш-функции
 	public int hash1(String str1) {
-		// 17
-		for (int i = 0; i < str1.length(); i++){
-			int code = (int) str1.charAt(i);
-		}
-		// реализация ...
-		return 0;
+		int entropy = 17;
+		return hashFun(str1, entropy);
 	}
+	
 
 	public int hash2(String str1) {
-		// 223
-		// реализация ...
-		return 0;
+		int entropy = 223;
+		return hashFun(str1, entropy);
 	}
 
+	/*
+	 * return index of value
+	 */
+	public int hashFun(String value, int entropy) {
+		char[] charArr = new char[value.length()];
+		int sum = 0;
+		for (int i = 0; i < value.length(); i++){
+			charArr[i] = value.charAt(i);
+			// cast char to int
+			sum = ((sum*entropy) + charArr[i] - '0') % filter_len;
+		}
+		return sum;
+	}
+	
 	public void add(String str1) {
+		int indexFirst = hash1(str1);
+		int indexSecond = hash2(str1);
+		filter[indexFirst] = 1;
+		filter[indexSecond] = 1;
 		// добавляем строку str1 в фильтр
 	}
 
 	public boolean isValue(String str1) {
 		// проверка, имеется ли строка str1 в фильтре
-		return false;
+		int indexFirst = hash1(str1);
+		int indexSecond = hash2(str1);
+		if(filter[indexFirst]==1 && filter[indexSecond] == 1){
+			return true;
+		}else{
+			return false;	
+		}
 	}
 }
---
+++ src/com/sabeshkin/bloomfilter/BloomFilterTests.java
@@ -0,0 +1,63 @@
+package com.sabeshkin.bloomfilter;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class BloomFilterTests {
+
+	
+	public static String moveChain(String str){
+		String lastLetter = str.substring(str.length()-1);
+		String movedChain = lastLetter + str.substring(0, str.length()-1);
+		return movedChain;
+	}
+	
+	@Test
+	public void test_add_isValue_true() {
+		BloomFilter bloomFilter = new BloomFilter(32);
+		bloomFilter.add("0123456789");
+		assertEquals(
+				true,
+				bloomFilter.isValue("0123456789"));
+	}
+	
+	@Test
+	public void test_add_isValue_false() {
+		BloomFilter bloomFilter = new BloomFilter(32);
+		bloomFilter.add("0123456789");
+		assertEquals(
+				false,
+				bloomFilter.isValue("7123456789"));
+	}
+	
+	@Test
+	public void test_add_tenDifferentValues_isValue_true() {
+		BloomFilter bloomFilter = new BloomFilter(32);
+		String str = "0123456789";
+		bloomFilter.add(str);
+		for(int i = 0; i<9; i++){
+			str = moveChain(str);
+			bloomFilter.add(str);
+		}	
+		assertEquals(
+				true,
+				bloomFilter.isValue("8901234567"));
+	}
+	
+	@Test
+	public void test_add_tenDifferentValues_isValue_false() {
+		BloomFilter bloomFilter = new BloomFilter(32);
+		String str = "0123456789";
+		bloomFilter.add(str);
+		for(int i = 0; i<9; i++){
+			str = moveChain(str);
+			bloomFilter.add(str);
+		}	
+		assertEquals(
+				false,
+				bloomFilter.isValue("9901234567"));
+	}
+	
+}
