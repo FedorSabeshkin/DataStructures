595100b90bd18ad9e96545f8ec362e1ec24fddcc
FedorSabeshkin <fsabeshkin@mail.ru>
2021-07-28 08:13:40 +0300

    add tests hits

--- src/com/sabeshkin/nativecache/NativeCacheTests.java
+++ src/com/sabeshkin/nativecache/NativeCacheTests.java
@@ -4,60 +4,79 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
-
 public class NativeCacheTests {
-
-
 
 	@Test
 	public void test_isKey_phrase16_isFalse() {
 		NativeCache nativeCache = new NativeCache(17, String.class);
-		for(int i = 0; i<17; i++)
-			nativeCache.put("phrase"+i,"Hello"+i);
-		
-		for(int i = 0; i<16; i++)
-			nativeCache.get("phrase"+i);
-		nativeCache.put("newKey","Bob");
-		boolean  isKey = nativeCache.isKey("phrase16");
+		for (int i = 0; i < 17; i++)
+			nativeCache.put("phrase" + i, "Hello" + i);
+
+		for (int i = 0; i < 16; i++)
+			nativeCache.get("phrase" + i);
+		nativeCache.put("newKey", "Bob");
+		boolean isKey = nativeCache.isKey("phrase16");
 		assertEquals(
 				false,
 				isKey);
 	}
-	
+
 	@Test
 	public void test_twoRequests_isKey_phrase16_isFalse() {
 		NativeCache nativeCache = new NativeCache(17, String.class);
-		for(int i = 0; i<17; i++)
-			nativeCache.put("phrase"+i,"Hello"+i);
-		
-		for(int i = 0; i<17; i++)
-			nativeCache.get("phrase"+i);
-		for(int i = 0; i<16; i++)
-			nativeCache.get("phrase"+i);
-		nativeCache.put("newKey","Bob");
-		boolean  isKey = nativeCache.isKey("phrase16");
+		for (int i = 0; i < 17; i++)
+			nativeCache.put("phrase" + i, "Hello" + i);
+
+		for (int i = 0; i < 17; i++)
+			nativeCache.get("phrase" + i);
+		for (int i = 0; i < 16; i++)
+			nativeCache.get("phrase" + i);
+		nativeCache.put("newKey", "Bob");
+		boolean isKey = nativeCache.isKey("phrase16");
 		assertEquals(
 				false,
 				isKey);
 	}
-	
-	
+
+	@Test
+	public void test_elevenRequests_isKey_phrase11_isFalse() {
+		NativeCache nativeCache = new NativeCache(17, String.class);
+		for (int i = 0; i < 17; i++){
+			nativeCache.put("phrase" + i, "Hello" + i);
+		}
+		// require all keys ten times
+		for (int k = 0; k < 10; k++){
+			for (int i = 0; i < 17; i++){
+				nativeCache.get("phrase" + i);
+			}
+		}
+		// require all keys exclude "phrase11"
+		for (int i = 0; i < 11; i++)
+			nativeCache.get("phrase" + i);
+		for (int i = 12; i < 17; i++)
+			nativeCache.get("phrase" + i);
+		nativeCache.put("newKey", "Bob");
+		boolean isKey = nativeCache.isKey("phrase11");
+		assertEquals(
+				false,
+				isKey);
+	}
+
 	@Test
 	public void test_getFieldValue_isHello() {
 		NativeCache nativeCache = new NativeCache(17, String.class);
-		nativeCache.put("phrase","Hello");
+		nativeCache.put("phrase", "Hello");
 		nativeCache.put("part", "World");
 		String valueByKey = (String) nativeCache.get("phrase");
 		assertEquals(
 				"Hello",
 				valueByKey);
 	}
-	
+
 	@Test
 	public void test_checkCollision_isCat() {
 		NativeCache nativeCache = new NativeCache(17, String.class);
-		nativeCache.put("phrase","Hello");
+		nativeCache.put("phrase", "Hello");
 		nativeCache.put("part", "World");
 		nativeCache.put("phrase", "Cat");
 		String valueByKey = (String) nativeCache.get("phrase");
@@ -65,11 +84,11 @@ public class NativeCacheTests {
 				"Cat",
 				valueByKey);
 	}
-	
+
 	@Test
 	public void test_checkCollision_isNotHello() {
 		NativeCache nativeCache = new NativeCache(17, String.class);
-		nativeCache.put("phrase","Hello");
+		nativeCache.put("phrase", "Hello");
 		nativeCache.put("part", "World");
 		nativeCache.put("phrase", "Cat");
 		String valueByKey = (String) nativeCache.get("phrase");
@@ -77,40 +96,39 @@ public class NativeCacheTests {
 				"Hello",
 				valueByKey);
 	}
-	
-	
+
 	@Test
 	public void test_checkCollision_isHello5() {
 		NativeCache nativeCache = new NativeCache(11, String.class);
-		for(int i = 0; i<11; i++){
-			if(i==10){
+		for (int i = 0; i < 11; i++){
+			if(i == 10){
 				int kek = 0;
 			}
-			nativeCache.put("phrase"+i,"Hello"+i);
+			nativeCache.put("phrase" + i, "Hello" + i);
 		}
 		String valueByKey = (String) nativeCache.get("phrase4");
 		assertEquals(
 				"Hello4",
 				valueByKey);
 	}
-	
+
 	@Test
 	public void test_isKey_phrase_isTrue() {
 		NativeCache nativeCache = new NativeCache(17, String.class);
-		for(int i = 0; i<17; i++)
-			nativeCache.put("phrase"+i,"Hello"+i);
+		for (int i = 0; i < 17; i++)
+			nativeCache.put("phrase" + i, "Hello" + i);
 		boolean isKey = nativeCache.isKey("phrase5");
 		assertEquals(
 				true,
 				isKey);
 	}
-	
+
 	@Test
 	public void test_isKey_capWord_isFalse() {
 		NativeCache nativeCache = new NativeCache(17, String.class);
-		for(int i = 0; i<17; i++)
-			nativeCache.put("phrase"+i,"Hello"+i);
-		boolean  isKey = nativeCache.isKey("capWord");
+		for (int i = 0; i < 17; i++)
+			nativeCache.put("phrase" + i, "Hello" + i);
+		boolean isKey = nativeCache.isKey("capWord");
 		assertEquals(
 				false,
 				isKey);
