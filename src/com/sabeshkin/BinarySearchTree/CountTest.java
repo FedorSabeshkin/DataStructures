fe971800eb9acf90702362af2fa0a262960b7080
FedorSabeshkin <fsabeshkin@mail.ru>
2022-03-05 20:45:36 +0300

    add tests for Count()

--- src/com/sabeshkin/BinarySearchTree/CountTest.java
+++ src/com/sabeshkin/BinarySearchTree/CountTest.java
@@ -27,7 +27,6 @@ class CountTest {
     }
 
 
-
     // 4. в Дереве из 5-ти элементов, находит нужный узел как в середине
     @Test
     public void findNodeByKey_Found_In_Middle() {
@@ -35,10 +34,10 @@ class CountTest {
 
         BSTNode<Integer> root = new BSTNode(8, 8, null);
         BST tree = new BST(root);
-        tree.AddKeyValue(4,4);
-        tree.AddKeyValue(12,12);
-        tree.AddKeyValue(2,2);
-        tree.AddKeyValue(6,6);
+        tree.AddKeyValue(4, 4);
+        tree.AddKeyValue(12, 12);
+        tree.AddKeyValue(2, 2);
+        tree.AddKeyValue(6, 6);
 
         assertEquals(5, tree.Count());
 
@@ -53,13 +52,56 @@ class CountTest {
         BSTNode<Integer> root = new BSTNode(8, 8, null);
         BST tree = new BST(root);
 
-        tree.AddKeyValue(4,4);
-        tree.AddKeyValue(12,12);
-        tree.AddKeyValue(2,2);
-        tree.AddKeyValue(6,6);
+        tree.AddKeyValue(4, 4);
+        tree.AddKeyValue(12, 12);
+        tree.AddKeyValue(2, 2);
+        tree.AddKeyValue(6, 6);
 
         tree.DeleteNodeByKey(4);
         tree.DeleteNodeByKey(12);
         assertEquals(3, tree.Count());
+    }
+
+    @Test
+    public void Count_Classical_Way_To_Add_Child() {
+        // 5 узлов
+        BSTNode<Integer> root = new BSTNode(8, 8, null);
+        BST tree = new BST(root);
+
+        //first
+        BSTNode leaf_4 = new BSTNode(4, 4, root);
+        root.LeftChild = leaf_4;
+        BSTNode leaf_12 = new BSTNode(12, 12, root);
+        root.RightChild = leaf_12;
+
+        //second left
+        BSTNode leaf_2 = new BSTNode(2, 2, leaf_4);
+        leaf_4.LeftChild = leaf_2;
+        BSTNode leaf_6 = new BSTNode(6, 6, leaf_4);
+        leaf_4.RightChild = leaf_6;
+
+        assertEquals(5, tree.Count());
+    }
+
+    @Test
+    public void Count_Classical_Way_To_Add_Child_After_Delete_One_Child() {
+        // 5 узлов
+        BSTNode<Integer> root = new BSTNode(8, 8, null);
+        BST tree = new BST(root);
+
+        //first
+        BSTNode leaf_4 = new BSTNode(4, 4, root);
+        root.LeftChild = leaf_4;
+        BSTNode leaf_12 = new BSTNode(12, 12, root);
+        root.RightChild = leaf_12;
+
+        //second left
+        BSTNode leaf_2 = new BSTNode(2, 2, leaf_4);
+        leaf_4.LeftChild = leaf_2;
+        BSTNode leaf_6 = new BSTNode(6, 6, leaf_4);
+        leaf_4.RightChild = leaf_6;
+
+        tree.DeleteNodeByKey(4);
+        assertEquals(4, tree.Count());
     }
 }
