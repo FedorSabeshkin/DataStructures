da65e1528c70df7cfc3e9f5905def31376249aa5
FedorSabeshkin <fsabeshkin@mail.ru>
2022-03-05 23:01:40 +0300

    add tests to DeleteNode()

--- src/com/sabeshkin/BinarySearchTree/DeleteNodeByKeyTest.java
+++ src/com/sabeshkin/BinarySearchTree/DeleteNodeByKeyTest.java
@@ -108,4 +108,52 @@ class DeleteNodeByKeyTest {
         tree.DeleteNodeByKey(4);
         assertNotEquals(4, root.LeftChild.NodeKey);
     }
+
+    @Test
+    public void DeleteNodeByKey_Remove_Leaf_4_Check_Children() {
+
+        // 5 узлов
+        BSTNode<Integer> root = new BSTNode(8, 8, null);
+        BST tree = new BST(root);
+
+        //first
+        BSTNode leaf_4 = new BSTNode(4, 4, root);
+        root.LeftChild = leaf_4;
+        BSTNode leaf_12 = new BSTNode(12, 12, root);
+        root.RightChild = leaf_12;
+
+        //second left
+        BSTNode leaf_2 = new BSTNode(2, 2, leaf_4);
+        leaf_4.LeftChild = leaf_2;
+        BSTNode leaf_6 = new BSTNode(6, 6, leaf_4);
+        leaf_4.RightChild = leaf_6;
+
+        tree.DeleteNodeByKey(4);
+
+        assertEquals(6, root.LeftChild.NodeKey);
+    }
+
+    @Test
+    public void DeleteNodeByKey_Remove_Leaf_4_Check_Children_Without_Parent() {
+
+        // 5 узлов
+        BSTNode<Integer> root = new BSTNode(8, 8, null);
+        BST tree = new BST(root);
+
+        //first
+        BSTNode leaf_4 = new BSTNode(4, 4, root);
+        root.LeftChild = leaf_4;
+        BSTNode leaf_12 = new BSTNode(12, 12, root);
+        root.RightChild = leaf_12;
+
+        //second left
+        BSTNode leaf_2 = new BSTNode(2, 2, leaf_4);
+        leaf_4.LeftChild = leaf_2;
+        BSTNode leaf_6 = new BSTNode(6, 6, leaf_4);
+        leaf_4.RightChild = leaf_6;
+
+        tree.DeleteNodeByKey(4);
+
+        assertEquals(8, root.LeftChild.Parent.NodeKey);
+    }
 }
